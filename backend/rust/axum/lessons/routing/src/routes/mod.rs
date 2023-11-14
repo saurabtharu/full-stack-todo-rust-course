8e0a70abde98409f0d0ca281c5fed487b29a3388
saurabtharu <saurab.tharu2@gmail.com>
2023-11-14 07:37:47 +0545

    Full Stack Learn: routing - CORS middleware

--- backend/rust/axum/lessons/routing/src/routes/mod.rs
+++ backend/rust/axum/lessons/routing/src/routes/mod.rs
@@ -6,9 +6,11 @@ mod path_variables;
 mod query_params;
 
 use axum::{
+    http::Method,
     routing::{get, post},
     Router,
 };
+use tower_http::cors::{Any, CorsLayer};
 
 use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
@@ -18,6 +20,10 @@ use path_variables::{hardcoded_path, path_variables};
 use query_params::query_params;
 
 pub fn create_routes() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
@@ -27,4 +33,5 @@ pub fn create_routes() -> Router {
         .route("/path_variables/:id", get(path_variables))
         .route("/query_params", get(query_params))
         .route("/mirror_custom_header", get(mirror_custom_header))
+        .layer(cors)
 }
