29a767b83b7acaf1ba5c36444019392766136c44
saurabtharu <saurab.tharu2@gmail.com>
2023-11-16 22:10:18 +0545

    Full Stack Learn: Sea-ORM -- atomic and partial update task as well as deleting task

--- backend/rust/axum/lessons/data/src/routes/mod.rs
+++ backend/rust/axum/lessons/data/src/routes/mod.rs
@@ -1,19 +1,25 @@
 pub mod create_task;
 pub mod custom_json_extrator;
+pub mod delete_task;
 pub mod get_tasks;
 pub mod hello_world;
+pub mod partial_update_task;
+pub mod update_tasks;
 pub mod validate_with_serde;
 
 use axum::{
-    routing::{get, post},
+    routing::{delete, get, patch, post, put},
     Extension, Router,
 };
 
 use create_task::create_task;
 use custom_json_extrator::custom_json_extrator;
+use delete_task::delete_task;
 use get_tasks::{get_all_tasks, get_one_task};
 use hello_world::hello_world;
+use partial_update_task::partial_update;
 use sea_orm::DatabaseConnection;
+use update_tasks::atomic_update;
 use validate_with_serde::validate_with_serde;
 
 pub async fn create_routes(database: DatabaseConnection) -> Router {
@@ -24,5 +30,8 @@ pub async fn create_routes(database: DatabaseConnection) -> Router {
         .route("/tasks", post(create_task))
         .route("/tasks", get(get_all_tasks))
         .route("/tasks/:task_id", get(get_one_task))
+        .route("/tasks/:task_id", put(atomic_update))
+        .route("/tasks/:task_id", patch(partial_update))
+        .route("/tasks/:task_id", delete(delete_task))
         .layer(Extension(database))
 }
